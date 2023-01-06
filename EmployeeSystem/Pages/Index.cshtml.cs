fdea703ba27cc58ac39b987b688f9c4fc6a15759
josem <josem@DESKTOP-RFNME1A>
2023-01-06 11:17:43 -0700

    Agregar archivos de proyecto.

---
+++ EmployeeSystem/Pages/Index.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using EmployeeSystem.Pages.Models;
+namespace EmployeeSystem.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<EmployeeModel> Employees { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void OnGet()
+        {
+            Employees = GetEmployees(Name);
+        }
+
+        private List<EmployeeModel> GetEmployees(string name)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "DESKTOP-RFNME1A"; // Replace with the server name or address
+            builder.InitialCatalog = "Employeesystem"; // Replace with the database name
+            builder.IntegratedSecurity = true;
+
+            string connectionString = builder.ConnectionString;
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Employee WHERE (@name IS NULL OR Name = @name) ORDER BY BornDate ASC";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    //If there is no name, then the parameter is set to dbnull
+                    if (string.IsNullOrEmpty(name))
+                    {
+                        command.Parameters.AddWithValue("@name", DBNull.Value);
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("@name", name ?? string.Empty);
+                    }
+
+
+                    List<EmployeeModel> employees = new List<EmployeeModel>();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            EmployeeModel employee = new EmployeeModel
+                            {
+                                ID = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                LastName = reader.GetString(2),
+                                RFC = reader.GetString(3),
+                                BornDate = reader.GetDateTime(4),
+                                Status = reader.GetString(5)
+                            };
+                            employees.Add(employee);
+                        }
+                    }
+                    return employees;
+                }
+            }
+        }
+    }
+}
---
+++ EmployeeSystem/Pages/Models/EmployeeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeSystem.Pages.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string RFC { get; set; }
+        public DateTime BornDate { get; set; }
+        public string Status { get; set; }
+    }
+}
